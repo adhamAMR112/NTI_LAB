c375588329ac1028bbfed7e4639cf9524e610829
adhamAMR112 <adham.beihry@gmail.com>
2026-07-27 00:33:11 +0300

    Add files via upload

---
+++ Lab06r/COUNTER.v
@@ -0,0 +1,29 @@
+module counter #(
+    parameter WIDTH = 5
+)(
+    input                   clk,
+    input                   rst,
+    input                   load,
+    input                   enab,
+    input  [WIDTH-1:0]      cnt_in,
+    output reg [WIDTH-1:0]  cnt_out
+);
+
+reg [WIDTH-1:0] next_cnt;
+
+always @(*) begin
+    if (rst)
+        next_cnt = {WIDTH{1'b0}};
+    else if (load)
+        next_cnt = cnt_in;
+    else if (enab)
+        next_cnt = cnt_out + 1'b1;
+    else
+        next_cnt = cnt_out;
+end
+
+always @(posedge clk) begin
+    cnt_out <= next_cnt;
+end
+
+endmodule
